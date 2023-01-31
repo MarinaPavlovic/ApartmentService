19035b1d0adacc7b65e22e0a43e274edb500c01b
MarinaPavlovic <marina.pavlovic7991@gmail.com>
2023-01-31 15:30:25 +0100

    update

--- src/main/java/rs/ac/singidunum/apartment/repository/IApartmentAvDaysRepository.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.singidunum.apartment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rs.ac.singidunum.apartment.entity.ApartmentAvailableDaysEntity;
-
-public interface IApartmentAvDaysRepository extends JpaRepository<ApartmentAvailableDaysEntity, Integer> {
-}
